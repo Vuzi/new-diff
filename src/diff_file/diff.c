de51d28ffeae3da1ef7d417411e2de03ec34814b
Vuzi <vuzi@free.fr>
2013-10-27 01:58:01 +0200

    Ajout de -y et de -u
    Pas encore fonctionnel, code non testé (Mais l'idée semble correcte).
    Vérification + correction demain.

--- src/diff_file/diff.c
+++ src/diff_file/diff.c
@@ -233,7 +233,11 @@ int diff_file(const char* f1_name, const char* f2_name) {
             /* Fichiers différents */
             else {
                 ret = 0;
-                if(p->report_identical_files)
+
+                if(p->o_style == COLUMNS) // Même si le fichier est identique, il faut l'afficher
+                    diff_display(diff_list, i1, i2);
+
+                if(p->report_identical_files) // Doit s'ajouter à l'affichage par colonnes
                     printf("Files %s and %s are identical\n", f1_name, f2_name);
             }
 
